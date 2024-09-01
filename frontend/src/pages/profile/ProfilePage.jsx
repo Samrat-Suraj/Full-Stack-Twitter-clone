d18f6b7f53a9ea7c36373c00906c5e7cc4d7ea52
Samrat-Suraj <143329360+Samrat-Suraj@users.noreply.github.com>
2024-09-01 23:26:19 +0530

    Update ProfilePage.jsx

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -175,7 +175,7 @@ const ProfilePage = () => {
 											<>
 												<FaLink className='w-3 h-3 text-slate-500' />
 												<a
-													href='https://youtube.com/@asaprogrammer_'
+													href=''
 													target='_blank'
 													rel='noreferrer'
 													className='text-sm text-blue-500 hover:underline'
